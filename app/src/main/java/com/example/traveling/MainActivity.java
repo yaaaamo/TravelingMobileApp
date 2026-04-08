944afe52c11db79b637e2ddf4314ac01e4d1a363
Yagmur Aydemir <rumaydemir02@gmail.com>
2026-04-08 17:27:27 +0200

    main firebase

--- app/src/main/java/com/example/traveling/MainActivity.java
+++ app/src/main/java/com/example/traveling/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.traveling;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +9,23 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+// MainActivity.java
+import com.google.firebase.FirebaseApp;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // Firebase auto-initializes with google-services plugin
+        // Manual init only if needed:
+        // FirebaseApp.initializeApp(this);
+
+        // Verify connection
+        if (FirebaseApp.getInstance() != null) {
+            Log.d("Firebase", "Firebase is connected!");
+        }
     }
 }
