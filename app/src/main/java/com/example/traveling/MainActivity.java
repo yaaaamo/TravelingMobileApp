6c42580da6701c11b49d8d1473100bda8de21e39
kanzyyyyy <kanzyeg3@gmail.com>
2026-04-15 19:16:13 +0200

    added bottomnavbar and fragments

--- app/src/main/java/com/example/traveling/MainActivity.java
+++ app/src/main/java/com/example/traveling/MainActivity.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.traveling.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -17,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setCurrentFragment(new Home());
 
         // Firebase auto-initializes with google-services plugin
         // Manual init only if needed:
@@ -37,13 +39,39 @@ public class MainActivity extends AppCompatActivity {
                 navigateToLogin();
             }
         });
+        BottomNavigationView bot = findViewById(R.id.bottomNavigationView);
+        bot.setOnNavigationItemSelectedListener(item -> {
+            int id = item.getItemId();
+
+            if (id == R.id.share) {
+                setCurrentFragment(new Home());
+            } else if (id == R.id.maps) {
+                setCurrentFragment(new Maps());
+            } else if (id == R.id.path) {
+                setCurrentFragment(new TravelPath());
+            } else if (id == R.id.groups) {
+                setCurrentFragment(new Groups());
+            } else if (id == R.id.profile) {
+                setCurrentFragment(new Profile());
+            }
+
+            return true;
+        });
+
     }
     public void navigateToHome(){
+        setCurrentFragment(new Home());
     }
     public void navigateToLogin(){
         Intent intent = new Intent(this, LoginActivity.class); // or whatever your main screen is
         startActivity(intent);
 
+    }
+    private void setCurrentFragment(androidx.fragment.app.Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment_container, fragment)
+                .commit();
     }
 
 }
